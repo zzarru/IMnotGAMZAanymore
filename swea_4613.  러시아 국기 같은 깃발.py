3ee3286d5a2b80f06e9befafbdc18ab01bcaef93
zzarru_air <sn20180301@gmail.com>
2023-03-03 01:05:53 +0900

    국기 만들었다.

--- swea_4613.  러시아 국기 같은 깃발.py
+++ swea_4613.  러시아 국기 같은 깃발.py
@@ -1,5 +1,22 @@
 T = int(input())
 for test in range(1, T+1):
     N, M = map(int, input().split())
-    flag = [list(input()) for _ in range(N)]
+    flags = []
+    for _ in range(N):
+        flag = input()
+        flags.append(flag)
 
+    total = 0
+    for i in range(N-2):
+        for j in range(i + 1, N - 1):
+            cnt = 0
+            for k in range(i+1):
+                cnt += flags[k].count('W')
+            for k in range(i+1, j+1):
+                cnt += flags[k].count('B')
+            for k in range(j+1, N):
+                cnt += flags[k].count('R')
+
+            total = max(total, cnt)
+
+    print(f'#{test}', N*M-total)
